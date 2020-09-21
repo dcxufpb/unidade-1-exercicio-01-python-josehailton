28edaa4f856e4fc8980964e551ad1c9f4eb6fa2c
josehailton <71135811+josehailton@users.noreply.github.com>
2020-09-21 14:24:43 -0300

    Update cupom.py
    ... 02

--- cupom.py
+++ cupom.py
@@ -20,5 +20,4 @@ def imprime_dados_loja():
     observacao
     s04 = ("CNPJ: %s" % cnpj)
     s05 = ("IE: %s" % inscricao_estadual)
-    stringResultado = "{}\n{}\n{}\n{}\n{}\n{}\n{}".format(nome_loja, s01, s02, s03, observacao, s04, s05)
-    return print(stringResultado)
+    return print("{}\n{}\n{}\n{}\n{}\n{}\n{}".format(nome_loja, s01, s02, s03, observacao, s04, s05))
