78629680afe4e183778f2f696a51ae062fc694d0
josehailton <71135811+josehailton@users.noreply.github.com>
2020-09-21 14:16:54 -0300

    Update cupom.py
    Havia faltado um {} na string de resultado.

--- cupom.py
+++ cupom.py
@@ -1,15 +1,3 @@
-nome_loja = "Arcos Dourados Com. de Alimentos LTDA"
-logradouro = "Av. Projetada Leste"
-numero = 500
-complemento = "EUC F32/33/34"
-bairro = "Br. Sta Genebra"
-municipio = "Campinas"
-estado = "SP"
-cep = "13080-395"
-telefone = "(19) 3756-7408"
-observacao = "Loja 1317 (PDP)"
-cnpj = "42.591.651/0797-34"
-inscricao_estadual = "244.898.500.113"
 def imprime_dados_loja():
     # Implemente aqui
     nome_loja
@@ -19,5 +7,5 @@ def imprime_dados_loja():
     observacao
     s04 = ("CNPJ: %s" % cnpj)
     s05 = ("IE: %s" % inscricao_estadual)
-    stringResultado = "{}\n{}\n{}\n{}\n{}\n{}".format(nome_loja, s01, s02, s03, observacao, s04, s05)
+    stringResultado = "{}\n{}\n{}\n{}\n{}\n{}\n{}".format(nome_loja, s01, s02, s03, observacao, s04, s05)
     return print(stringResultado)
